73cd0d4cc53784c66192ac61e9df15a951f65fa8
mai1346 <maihaoyuan@gmail.com>
2022-01-26 17:30:17 +0800

    blackjack 修改初始化函数，完成没有usable的情景

--- chapter05/blackjack_mine.py
+++ chapter05/blackjack_mine.py
@@ -4,6 +4,8 @@
 import numpy as np
 from tqdm import trange
 
+np.random.seed(5)
+
 
 def get_a_card():
     card = np.random.choice(13)+1
@@ -14,64 +16,68 @@ def get_a_card():
 def initialize():
     player_sum = 0
     dealer_sum = 0
-    player_sum += get_a_card()
-    player_sum += get_a_card()
+    while player_sum < 12:
+        player_sum += get_a_card()
     dealer_show = get_a_card()
     dealer_sum += dealer_show
     dealer_sum += get_a_card()
     return player_sum, dealer_sum, dealer_show
 
 
-def single_episode(player_s, dealer_show, dealer_sum):
-    intermediate_player_s = [player_s]
-    intermediate_dealer_s = [dealer_show]
+def single_episode(player_s, dealer_sum, dealer_show):
+    intermediate_player_s = []
+    intermediate_dealer_s = []
     # player move
     while player_s < 20:
-        player_s += get_a_card()
         intermediate_player_s.append(player_s)
         intermediate_dealer_s.append(dealer_show)
+        player_s += get_a_card()
         if player_s > 21:
             reward = -1
+            print(f"player:{player_s};dealer:{dealer_show}; reward:{reward}")
             return reward, intermediate_player_s, intermediate_dealer_s
-        elif player_s in [20,21]:
-            break
+        # elif player_s in [20,21]:
+        #     break
+    intermediate_player_s.append(player_s)
+    intermediate_dealer_s.append(dealer_show)
     # dealer move
     while dealer_sum < 17:
         dealer_sum += get_a_card()
         if dealer_sum > 21:
             reward = 1
+            print(f"player:{player_s};dealer:{dealer_show}; reward:{reward}")
             return reward, intermediate_player_s, intermediate_dealer_s
-        elif dealer_sum >= 17:
-            break
-
+        # elif dealer_sum >= 17:
+        #     break
     if player_s > dealer_sum:
         reward = 1
-        return reward, intermediate_player_s, intermediate_dealer_s
     elif player_s < dealer_sum:
         reward = -1
-        return reward, intermediate_player_s, intermediate_dealer_s
     else:
         reward = 0
-        return reward, intermediate_player_s, intermediate_dealer_s
+    print(f"player:{player_s};dealer:{dealer_show}; reward:{reward}")
+    return reward, intermediate_player_s, intermediate_dealer_s
 
 
 if __name__ == '__main__':
     # state = [10, 1, 9]
-    player_sum = np.arange(12, 21 + 1)
-    dealer_show = np.arange(1, 10 + 1)
+    player_state = np.arange(12, 21 + 1)
+    dealer_state = np.arange(1, 10 + 1)
     # player_usable = np.arange(0,1+1)
-    state_values = np.zeros((len(dealer_show), len(player_sum)))
-    for p_sum in player_sum:
-        for d_show in dealer_show:
-            state = [p_sum, d_show]
-            print(state)
-            state_sum = 0
-            for i in trange(10000):
-                state_sum += single_episode(state)
-            state_value = state_sum / 10000
-            state_values[d_show-10,p_sum-12] = state_value
+    state_values = np.zeros((len(player_state), len(dealer_state)))
+    state_counts = np.zeros((len(player_state), len(dealer_state)))
 
-    # single_episode([20, 1])
+    for i in trange(10000):
+        player_sum, dealer_sum, dealer_show = initialize()
+        reward, inter_player_s, inter_dealer_s = single_episode(player_sum, dealer_sum, dealer_show)
+        # print(reward)
+        player_idx_arr = np.array(inter_player_s) - 12
+        dealer_idx_arr = np.array(inter_dealer_s) - 1
+        state_counts[player_idx_arr, dealer_idx_arr] += 1
+        state_values[player_idx_arr, dealer_idx_arr] += reward
+
+    state_values /= state_counts
+
 
 
 
