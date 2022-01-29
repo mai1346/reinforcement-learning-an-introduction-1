3fe0a3dca470260db926cb005cf75f8e08bf09d7
mai1346 <maihaoyuan@gmail.com>
2022-01-29 11:38:10 +0800

    修改超过21点时的算法逻辑，实现随机初始化state，每个episode开始步骤随机

--- chapter05/blackjack_mine.py
+++ chapter05/blackjack_mine.py
@@ -42,24 +42,33 @@ def greedy(player_sum, dealer_show, usable, state_values, state_counts):
     return action
 
 
-def initialize():
-    player_sum = 0
-    dealer_sum = 0
-    usable = 0
-    dealer_usable = 0
-    while player_sum < 12:
-        card = get_a_card()
-        usable += card == 11
-        player_sum += card
-        if player_sum > 21:
-            player_sum -= 10
-            usable -= 1
-    dealer_show = get_a_card()
-    dealer_sum += dealer_show
-    dealer_usable += dealer_show == 11
-    dealer_hide = get_a_card()
-    dealer_sum += dealer_hide
-    dealer_usable += dealer_hide == 11
+def initialize(random=True):
+    if random:
+        dealer_usable = 0
+        player_sum = np.random.randint(12,22)
+        dealer_show = np.random.randint(2,12)
+        usable = np.random.randint(0,2)
+        dealer_usable += dealer_show==11
+        dealer_sum = dealer_show + np.random.randint(2,12)
+        dealer_usable += dealer_sum==22
+    else:
+        player_sum = 0
+        dealer_sum = 0
+        usable = 0
+        dealer_usable = 0
+        while player_sum < 12:
+            card = get_a_card()
+            usable += card == 11
+            player_sum += card
+            if player_sum > 21:
+                player_sum -= 10
+                usable -= 1
+        dealer_show = get_a_card()
+        dealer_sum += dealer_show
+        dealer_usable += dealer_show == 11
+        dealer_hide = get_a_card()
+        dealer_sum += dealer_hide
+        dealer_usable += dealer_hide == 11
     if dealer_sum > 21:
         dealer_sum -= 10
         dealer_usable -= 1
@@ -67,13 +76,12 @@ def initialize():
 
 
 def single_episode(player_s, dealer_sum, dealer_show, usable, dealer_usable, state_values, state_counts,
-                   policy=stick_above):
+                   policy=stick_above, random_initial_action=False):
     intermediate_player_s = []
     intermediate_dealer_s = []
     intermediate_player_usable = []
     intermediate_action = []
-    hit = policy(player_s, dealer_show, usable, state_values, state_counts)
-    # hit =  policy[player_s-1]
+    hit = np.random.randint(0,2) if random_initial_action else policy(player_s, dealer_show, usable, state_values, state_counts)
     # player move
     while hit:
         intermediate_player_s.append(player_s)
@@ -83,19 +91,14 @@ def single_episode(player_s, dealer_sum, dealer_show, usable, dealer_usable, sta
         card = get_a_card()
         usable += card == 11
         player_s += card
+        while usable and player_s > 21:
+            player_s -= 10
+            usable -= 1
         if player_s > 21:
-            if usable:
-                player_s -= 10
-                usable -= 1
-                if player_s > 21:
-                    player_s -= 10
-                    usable -= 1
-                hit = policy(player_s, dealer_show, usable, state_values, state_counts)
-                continue
             reward = -1
             return reward, intermediate_player_s, intermediate_dealer_s, intermediate_player_usable, intermediate_action
-        hit = policy(player_s, dealer_show, usable, state_values, state_counts)
-
+        else:
+            hit = policy(player_s, dealer_show, usable, state_values, state_counts)
     intermediate_player_s.append(player_s)
     intermediate_dealer_s.append(dealer_show)
     intermediate_action.append(hit)
@@ -144,13 +147,12 @@ def monte_carlo_es(num_eps, exploring_starts=False):
     state_values = np.zeros((len(player_state), len(dealer_state), 2, 2))  # 初始化一个4维矩阵，分别储存
     state_counts = np.ones((len(player_state), len(dealer_state), 2, 2))
     for i in trange(num_eps):
-        if exploring_starts:
-            pass
-        else:
-            player_sum, dealer_sum, dealer_show, usable, dealer_usable = initialize()
+        # print(i)
+        player_sum, dealer_sum, dealer_show, usable, dealer_usable = initialize(random=exploring_starts)
+        policy = greedy if i else stick_above
         reward, inter_player_s, inter_dealer_s, usable, action = single_episode(player_sum, dealer_sum, dealer_show,
                                                                                 usable, dealer_usable, state_values,
-                                                                                state_counts, policy=greedy)
+                                                                                state_counts, policy=policy, random_initial_action=True)
         player_idx_arr = np.array(inter_player_s) - 12
         dealer_idx_arr = np.array(inter_dealer_s)
         dealer_idx_arr[dealer_idx_arr == 11] = 1  # 让11 重置为1
@@ -163,7 +165,7 @@ def monte_carlo_es(num_eps, exploring_starts=False):
 if __name__ == '__main__':
     player_state = np.arange(12, 21 + 1)
     dealer_state = np.arange(1, 10 + 1)
-    test = monte_carlo_es(500000)
+    test = monte_carlo_es(500000, exploring_starts=True)
     state_value_no_usable_ace = np.max(test[:, :, 0, :], axis=2)
     state_value_usable_ace = np.max(test[:, :, 1, :], axis=2)
 
